4ffeb70db23327925bbcda8494d58be2aff26495
Josephine Amos <josephinexamos@gmail.com>
2020-10-28 17:55:05 -0400

    changes

--- binary-search/binary-search.js
+++ binary-search/binary-search.js
@@ -2,7 +2,26 @@
 
 // Complete this algo
 const binarySearch = (array, target) => {
-	
+	//find the root by splitting the array and rounding up
+	let root = Math.floor(array.length / 2 )
+		//cut the array in half, lowest numbers on the left and highest on the right
+	let lowArray = array.slice(0, root)
+	let highArray = array.slice(root);
+	if (target === root) {
+		return true
+	} else if (target < root) {
+		if (lowArray.length < 2 && root !== target) {
+			return false
+		} else {
+			binarySearch(lowArray, target)
+		}
+	} else {
+		 if (binarySearch(highArray, target)) {
+			return true
+		} else {
+			return false
+		}
+	}
 };
 
 /*
